7b7f4f853f58e96739e1ed7fb13eacd97474761f
Saurabh Deoras <sdeoras@gmail.com>
2019-03-08 19:06:06 -0800

    updates for health api
    Signed-off-by: Saurabh Deoras <sdeoras@gmail.com>

--- gan/lambda.go
+++ gan/lambda.go
@@ -32,7 +32,7 @@ func init() {
 			return filepath.Join("/", input)
 		}
 
-		h := health.New(health.OutputProto, jwt.Manager)
+		h := health.NewProvider(health.OutputProto, jwt.Manager, nil)
 		h.Register(f(routeGen), nil)
 
 		router = httprouter.NewRouter()
